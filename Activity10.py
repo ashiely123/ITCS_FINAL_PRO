1d7176849af98644f152e40815ffa85aa5dec2fe
ashiely123 <ashlytubiera18@gmail.com>
2024-12-17 01:51:08 -0800

    Update Activity10.py

--- Activity10.py
+++ Activity10.py
@@ -1,5 +1,21 @@
-for x in range(0, 10):
-    print("hello world")
-    print("Welcome to dll foundation day!")
-    y = input("what's your name?-------->")
-    print(f"what's up {y}")
+DLL = input("Are you a student of DLL? (yes/no??) : ")
+
+if DLL.lower() == "yes":
+	print("Good Morning Student!","\nWelcome to the DLL Scholarship Form!")
+
+	yearlv = input("What is your current year in DLL?")
+	if yearlv.lower() == "freshman":
+		print("Hello Freshmen of DLL!")
+	elif yearlv.lower() == "freshmen":
+		print("Hello freshmen of DLL!") 
+	elif yearlv.lower() == "junior":
+		print("Konnichiwa Junior_kun of DLL!")
+	elif yearlv.lower() == "senior":
+		print("Welcome Senior of DLL!")
+	else: 
+			print("???")
+	youNeed = input("Do you need this scholarship?? (y/n) : ")
+	if youNeed.lower() == "yes":
+		print ("Scholarship Granted :}")
+	else:
+		print("Okay thanks for using it")
